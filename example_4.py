3cf66a04d6a4b819b816523812274060b73c36ff
David <david@schimansky-online.de>
2013-06-13 19:31:14 +0200

    error: y_new and y were pointing to the same object. Thus y was also modified by the median filtering process. y_new has to be a copy of y. Fix: Set y_new as deepcopy of y. Import deepcopy from copy.

--- example_4.py
+++ example_4.py
@@ -2,13 +2,14 @@
 # 4) but it looks like there is an issue in the plot - the red curve should
 # follow the blue points better. What is happening?
 
+from copy import deepcopy
 import numpy as np
 np.random.seed(12345)  # ensures the random values are always the same
 import matplotlib.pyplot as plt
 
 
 def median_filter(x, y, width):
-    y_new = y
+    y_new = deepcopy(y)
     for i in range(len(x)):
         y_new[i] = np.median(y[(x > x[i] - width * 0.5) &
                                (x < x[i] + width * 0.5)])
